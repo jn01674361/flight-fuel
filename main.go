711182673953138b0c7de19eae154d95991e268e
jn01674361 <joar.nykvist@gmail.com>
2019-08-12 17:48:13 +0200

    Add bootstrap

--- main.go
+++ main.go
@@ -150,6 +150,7 @@ func main() {
 				fromCountry,
 				toCountry,
 			})
-    })
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
     http.ListenAndServe(":8080", nil)
 }
